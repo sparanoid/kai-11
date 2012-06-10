86cc2d6980377a8ebc583a92bee89e29fad30d83
Tunghsiao Liu <info@sparanoid.com>
2012-06-11 01:58:52 +0800

    Upload links to postholic.com

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  */
 
 function add_twentyeleven_credits() {
-  echo '<a href="http://ray.io/rsrc/twentyeleven-kai/">Twenty Eleven Kai</a><span class="sep"> | </span>';
+  echo '<a href="http://postholic.com/rsrc/twentyeleven-kai/">Twenty Eleven Kai</a><span class="sep"> | </span>';
 }
 
 add_action('twentyeleven_credits', 'add_twentyeleven_credits');
