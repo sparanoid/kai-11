27069d622a5537af0c72ea9bdd086fe508bb0836
Tunghsiao Liu <info@sparanoid.com>
2012-10-14 15:58:44 +0800

    Load child theme stylesheet in a more graceful way to avoid CSS @import. More information about CSS @import: http://www.stevesouders.com/blog/2009/04/09/dont-use-import/

--- functions.php
+++ functions.php
@@ -1,6 +1,10 @@
 <?php
 /**
  * Twenty Eleven Kai functions and definitions
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven_Kai
+ * @since Twenty Eleven Kai 1.0
  */
 
 function add_twentyeleven_credits() {
@@ -8,4 +12,15 @@ function add_twentyeleven_credits() {
 }
 
 add_action('twentyeleven_credits', 'add_twentyeleven_credits');
+
+/**s
+ * Override Twenty Eleven stylesheet loading behavier and load Twenty Eleven Kai stylesheet
+ *
+ * @since Twenty Eleven Kai 1.5.1
+ */
+function twentyeleven_scripts_styles_override() {
+   wp_enqueue_style( 'twentyeleven-style', get_template_directory_uri() . '/style.css' );
+   wp_enqueue_style( 'twentyeleven-kai-style', get_stylesheet_directory_uri() . '/core.css' );
+ }
+add_action( 'wp_enqueue_scripts', 'twentyeleven_scripts_styles_override' );
 ?>
