3de469a6f97794e459bc80ad8be1a32c43238b3c
Tunghsiao Liu <info@sparanoid.com>
2012-12-13 16:04:08 +0800

    Override default Jetpack Infinite Scroll footer

--- functions.php
+++ functions.php
@@ -23,4 +23,15 @@ function twentyeleven_scripts_styles_override() {
    wp_enqueue_style( 'twentyeleven-kai-style', get_stylesheet_directory_uri() . '/core.css' );
  }
 add_action( 'wp_enqueue_scripts', 'twentyeleven_scripts_styles_override' );
+
+/**
+ * Override default Jetpack Infinite Scroll footer
+ *
+ * @since Twenty Eleven Kai 1.5.4
+ */
+function twentyeleven_kai_infinite_scroll_credit() {
+  return '<a href="http://postholic.com/rsrc/twentyeleven-kai/">Twenty Twelve Kai</a>';
+}
+
+add_filter( 'infinite_scroll_credit', 'twentyeleven_kai_infinite_scroll_credit' );
 ?>
