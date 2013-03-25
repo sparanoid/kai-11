07f784f51da456419c3000b2ce524b546e041a64
Tunghsiao Liu <t@sparanoid.com>
2013-03-25 10:22:25 +0800

    * Twenty Eleven Kai -> Kai 11 * Bump version

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-Twenty Eleven Kai
-=================
+Kai 11
+======
 
 The missing child theme of Twenty Eleven, A better interface based on Twenty Eleven, No need to change the default theme, no database option strings will be created. Install and go. You can have a look at the [demo][demo] first.
 
@@ -13,7 +13,7 @@ Installation
 License
 -------
 
-The license under which the Twenty Eleven Kai is released is the GPLv2 from the [Free Software Foundation][fsf].
+The license under which the Kai 11 is released is the GPLv2 from the [Free Software Foundation][fsf].
 
 [fsf]: http://www.fsf.org
-[demo]: http://postholic.com/twentyeleven-kai-demo/
+[demo]: http://postholic.com/kai-11-demo/
--- functions.php
+++ functions.php
@@ -1,48 +1,48 @@
 <?php
 /**
- * Twenty Eleven Kai functions and definitions
+ * Kai 11 functions and definitions
  *
  * @package WordPress
  * @subpackage Twenty_Eleven_Kai
- * @since Twenty Eleven Kai 1.0
+ * @since Kai 11 1.0
  */
 
-function add_twentyeleven_credits() {
-  echo '<a href="http://postholic.com/rsrc/twentyeleven-kai/">Twenty Eleven Kai</a><span class="sep"> | </span>';
+function add_kai_11_credits() {
+  echo '<a href="http://postholic.com/rsrc/kai-11/">Kai 11</a><span class="sep"> | </span>';
 }
 
-add_action('twentyeleven_credits', 'add_twentyeleven_credits');
+add_action('twentyeleven_credits', 'add_kai_11_credits');
 
 /**
- * Override Twenty Eleven stylesheet loading behavier and load Twenty Eleven Kai stylesheet
+ * Override Twenty Eleven stylesheet loading behavier and load Kai 11 stylesheet
  *
- * @since Twenty Eleven Kai 1.5.1
+ * @since Kai 11 1.5.1
  */
 function twentyeleven_scripts_styles_override() {
    wp_enqueue_style( 'twentyeleven-style', get_template_directory_uri() . '/style.css' );
-   wp_enqueue_style( 'twentyeleven-kai-style', get_stylesheet_directory_uri() . '/core.css' );
+   wp_enqueue_style( 'kai-11-style', get_stylesheet_directory_uri() . '/core.css' );
  }
 add_action( 'wp_enqueue_scripts', 'twentyeleven_scripts_styles_override' );
 
 /**
  * Override default Jetpack Infinite Scroll footer
  *
- * @since Twenty Eleven Kai 1.5.4
+ * @since Kai 11 1.5.4
  */
-function twentyeleven_kai_infinite_scroll_credit() {
-  return '<a href="http://postholic.com/rsrc/twentyeleven-kai/">Twenty Twelve Kai</a>';
+function kai_11_infinite_scroll_credit() {
+  return '<a href="http://postholic.com/rsrc/kai-11/">Twenty Twelve Kai</a>';
 }
 
-add_filter( 'infinite_scroll_credit', 'twentyeleven_kai_infinite_scroll_credit' );
+add_filter( 'infinite_scroll_credit', 'kai_11_infinite_scroll_credit' );
 
 /**
  * Initialize the update checker
  *
- * @since Twenty Eleven Kai 1.5.4
+ * @since Kai 11 1.5.4
  */
 require 'includes/theme-update-checker.php';
-$twentyeleven_kai_update_checker = new ThemeUpdateChecker(
-  'twentyeleven-kai',
-  'http://sparanoid.com/lab/wordpress/twentyeleven-kai.json'
+$kai_11_update_checker = new ThemeUpdateChecker(
+  'kai-11',
+  'http://sparanoid.com/lab/wordpress/kai-11.json'
 );
 ?>
